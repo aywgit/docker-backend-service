9d54bc85ed42ab91d706122cb886eb8dbe46cf23
Angela Wu <ange.y.wu@gmail.com>
2021-06-23 16:54:49 -0500

    servers connected

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
+console.log(__dirname);
+// app.use(express.json());
+// app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/');
+
+module.exports = app;
+
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const app = require('./app.js');
+const db = require('./mongo.js');
+const port = 3000;
 
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
