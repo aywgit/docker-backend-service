8a088ca307967161c5d0b506f38debf6f846f480
Angela Wu <ange.y.wu@gmail.com>
2021-07-03 11:58:07 -0500

    changed app.js back to mongodb://localhost:27017

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
 let ObjectId = require('mongodb').ObjectId;
 const assert = require('assert');
-const url = 'mongodb://mongodb:27017';
+const url = 'mongodb://localhost:27017';
 const dbName = 'questionAnswers';
 
 app.get('', (req, res) => {
